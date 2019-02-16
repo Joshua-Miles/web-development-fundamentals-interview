613131f5303601711d618996048441311e0cdb4a
joshua-miles <joshua@christetulit.org>
2019-02-16 15:56:29 -0600

    initial

---
+++ index.rb
@@ -0,0 +1,36 @@
+
+# 1. What is an Array, what is a Hash, and how are they different?   
+
+# For questions 2 and 3, consider the following array of hashes.
+
+heroes = [
+    {
+        name: 'Iron Man',
+        age: 48
+    },
+    {
+        name: 'Thor',
+        age: 1035
+    },
+    {
+        name: 'Hulk',
+        age: 49
+    },
+    {
+        name: 'Spiderman',
+        age: 15
+    }
+]
+
+# 2. Write a function which, given the above array of heroes as an argument,
+#    returns the oldest hero from the array.
+
+# 3. Write a function which, given the above array of heroes and the name of a 
+#    hero, returns the age of that hero.
+
+
+require 'pry'
+
+# Your Code Here
+
+binding.pry
